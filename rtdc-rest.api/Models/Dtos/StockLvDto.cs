d862ff7973e0daece0bce52fcdbe37297af43cc4
Mücahit Ceviz <invalid.10bjects@gmail.com>
2023-02-28 12:23:15 +0300

    added backgroundService and Dtos

---
+++ rtdc-rest.api/Models/Dtos/StockLvDto.cs
@@ -0,0 +1,16 @@
+﻿namespace rtdc_rest.api.Models.Dtos
+{
+    public class StockLvDto
+    {
+        public string dataSourceCode { get; set; }
+        public string manufacturerCode { get; set; }
+        public DateTime stockDate { get; set; }
+        public string productCode { get; set; }
+        public double itemQuantity { get; set; }
+        public int quantityInPackage { get; set; }
+        public double packageQuantity { get; set; }
+        public string itemBarcode { get; set; }
+        public string packageBarcode { get; set; }
+        public double listPrice { get; set; }
+    }
+}
